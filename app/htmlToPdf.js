bc6b459b2e5ec45fdb44915f5e66709b6c027b7a
wwei <wwei@humanlongevity.com>
2022-08-17 11:56:57 +0800

    push

--- app/htmlToPdf.js
+++ app/htmlToPdf.js
@@ -7,24 +7,23 @@ const Mustache = require('mustache');
 
 const convertHtmlToPDF = async (ctx)=>{
     const url = await getChart()
-    fs.readFile('demo.mustache',function(err,data){
-        const template= data.toString()
-        const  rendered = Mustache.render(template, {
-            name: 'Luke',
-            rich:'<h1>this is <i style="color: blue">rich</i> text</h1>',
-            // chart:'https://s3.bmp.ovh/imgs/2022/06/30/4143fa535837202b.png',
-            chart:url,
-        });
-        // console.log(rendered)
-        wkhtmltopdf(rendered, { pageSize: 'letter' })
-            .pipe(fs.createWriteStream('out.pdf'));
-    })
+     const  data = await fs.readFileSync('demo.mustache')
+    const template= data.toString()
+    const  rendered = Mustache.render(template, {
+        name: 'Luke',
+        rich:'<h1>this is <i style="color: blue">rich</i> text</h1>',
+        // chart:'https://s3.bmp.ovh/imgs/2022/06/30/4143fa535837202b.png',
+        chart:url,
+    });
+    // console.log(rendered)
+    ctx.set('Content-Type', 'application/pdf');
+    ctx.set("Access-Control-Allow-Origin", "*");
+    const stream =  await wkhtmltopdf('http://www.qq.com/', { pageSize: 'letter' })
+    ctx.body = stream;
     // const html = `<h1 style="color:red">${value}<img src="https://s3.bmp.ovh/imgs/2022/06/30/4143fa535837202b.png"/></h1>`
     // wkhtmltopdf('https://www.baidu.com', { pageSize: 'letter' })
     //     .pipe(fs.createWriteStream('out.pdf'));
-    ctx.body = {
-        status:0
-    };
+
 }
 module.exports ={
     convertHtmlToPDF
